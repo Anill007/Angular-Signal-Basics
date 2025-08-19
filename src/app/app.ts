ec2532b34cffa54ac74eda112db8df97b5efaf14
Anill007 <anil.123aaaaaa@gmail.com>
2025-08-19 10:01:19 +0545

    feat(component): app component with signals to get emoji list and search functionality - debounced signal as debouncedSedarch to implement optimized search - computed signal as filteredList to filter from emoji list - writable signal as searchInput to store search values

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal, WritableSignal, effect, computed, Signal } from '@angular/core';
+import { EmojisResource } from './service/emojis-resource';
+import { NgFor, NgIf } from '@angular/common';
+import { toSignal, toObservable } from '@angular/core/rxjs-interop';
+import { debounce, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('Angular-Signal-Basics');
+  private emojiResource = inject(EmojisResource);
+
+  private debouncedSearch!: Signal<string>;
+  protected searchInput: WritableSignal<string> = signal('');
+  protected emojiList = this.emojiResource.emojiList();
+  protected filteredList = computed(() => {
+    return this.emojiResource
+      .emojiList()
+      .value()
+      .filter((v) => {
+        const query = this.debouncedSearch();
+        if (!query) true;
+        return v.name.includes(query);
+      });
+  });
+
+  constructor() {
+    this.debouncedSearch = toSignal(toObservable(this.searchInput).pipe(debounceTime(1000)), {initialValue: ''});
+  }
+
+  protected search(searchEvent: Event) {
+    this.searchInput.set((searchEvent.target as HTMLInputElement).value);
+  }
 }
